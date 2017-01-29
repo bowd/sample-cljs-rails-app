74d8567cc298f8e2258d9fd3e851f2c8024907aa
Bogdan Dumitru <dumbogdan@gmail.com>
2017-01-29 19:51:50 +0200

    Setup app with cljs-rails for heroku deployment

---
+++ cljs/src/testappcljsrails/core.cljs
@@ -0,0 +1,11 @@
+(ns testappcljsrails.core
+  (:require [devtools.core :as devtools]
+            [clojure.browser.dom :as dom]))
+
+;; -- Debugging aids ----------------------------------------------------------
+(devtools/install!)       ;; we love https://github.com/binaryage/cljs-devtools
+(enable-console-print!)   ;; so that println writes to `console.log`
+
+(defn ^:export main
+  []
+  (dom/append (.-body js/document) (dom/element "div" "Hello from Clojurescript!")))
